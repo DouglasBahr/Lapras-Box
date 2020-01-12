28b4f28d03dfb941d5a5fe77b07200e184897511
DouglasB <douglasbahr1@gmail.com>
2020-01-12 14:57:56 -0800

    Intial Update to test over the air
    First commit

---
+++ main/main.py
@@ -0,0 +1,21 @@
+ from ota_update.main.ota_updater import OTAUpdater
+
+
+ def download_and_install_update_if_available():
+     o = OTAUpdater('https://github.com/DouglasBahr/Lapras-Box')
+     o.download_and_install_update_if_available('HOME-286C', 'tubscat1')
+
+
+ def start():
+     # your custom code goes here. Something like this: ...
+     # from main.x import YourProject
+     # project = YourProject()
+     # ...
+
+
+ def boot():
+     download_and_install_update_if_available()
+     start()
+
+
+ boot()
